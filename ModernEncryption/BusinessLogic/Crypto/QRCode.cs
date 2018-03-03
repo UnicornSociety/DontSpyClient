196e19df7d7f0b2ea29ed43edb6acb5fa9ca9022
Tobias Straub <hello@tobiasstraub.com>
2018-03-03 20:00:08 +0100

    Dirty QRCode Writer implementation

---
+++ ModernEncryption/BusinessLogic/Crypto/QRCode.cs
@@ -0,0 +1,23 @@
+﻿using SkiaSharp;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+using ZXing.SkiaSharp;
+
+namespace ModernEncryption.BusinessLogic.Crypto
+{
+    internal class QrCode
+    {
+        public SKBitmap Draw(string content, int width, int height) {
+            return new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    Width = width,
+                    Height = height
+                }
+            }.Write(content);
+        }
+    }
+}
