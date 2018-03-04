2c4f28294e12807761d2393eafc1e3b51b5ab013
Tobias Straub <hello@tobiasstraub.com>
2018-03-04 09:49:56 +0100

    Implemented camera access functionalities

--- ModernEncryption/BusinessLogic/Crypto/QRCode.cs
+++ ModernEncryption/BusinessLogic/Crypto/QRCode.cs
@@ -1,4 +1,7 @@
-﻿using SkiaSharp;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Plugin.Media;
+using SkiaSharp;
 using ZXing;
 using ZXing.Common;
 using ZXing.QrCode;
@@ -18,6 +21,20 @@ namespace ModernEncryption.BusinessLogic.Crypto
                     Height = height
                 }
             }.Write(content);
+        }
+
+        public async Task<Stream> ReadViaCamera(long channelId)
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported) return null;
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                DefaultCamera = Plugin.Media.Abstractions.CameraDevice.Front,
+                Directory = "TemporaryKeys",
+                Name = channelId + ".jpg"
+            });
+            
+            return file?.GetStream();
         }
     }
 }
